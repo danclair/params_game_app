f9c71f5beb02af51b1af0c2b27d7fccf832a220f
Dan Clair <danclair@me.com>
2020-03-03 21:03:29 -0600

    Add uppercase method in controller

--- app/controllers/api/params_game_controller.rb
+++ app/controllers/api/params_game_controller.rb
@@ -1,5 +1,8 @@
 class Api::ParamsGameController < ApplicationController
   def query_params_game_action
+    input_name = params["my_name"]
+
+    @output_msg = "#{input_name}"
     render "params_game.json.jb"
   end
 end
