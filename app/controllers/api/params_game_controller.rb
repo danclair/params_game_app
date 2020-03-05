a10a78a48454464d64a521693f547d0a7087206d
Dan Clair <danclair@me.com>
2020-03-04 19:40:28 -0600

    Add body params version of guess number, and body login

--- app/controllers/api/params_game_controller.rb
+++ app/controllers/api/params_game_controller.rb
@@ -1,8 +1,32 @@
 class Api::ParamsGameController < ApplicationController
   def query_params_game_action
-    input_name = params["my_name"]
+    input_name = params["thevariable"]
 
     @output_msg = "#{input_name}"
     render "params_game.json.jb"
+  end
+
+  def guess_action
+    winning_number = 34
+    input_guess = params["theguess"].to_i
+    if input_guess > winning_number
+      @message = "Pick lower!"
+    elsif input_guess < winning_number
+      @message = "Pick higher!"
+    else
+      @message = "You win!"
+    end
+    render "guess_number.json.jb"
+  end
+
+  def login_action
+    input_username = params["username"]
+    input_password = params["password"]
+    if input_username == "hugh" && input_password == "swordfish"
+      @message = "Valid credentials"
+    else
+      @message = "Invalid credentials"
+    end
+    render "body_params.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/query_params_game" => "params_game#query_params_game_action"
+    get "/query_params_url" => "params_game#query_params_game_action"
+    get "/segment_params_url/:thevariable" => "params_game#query_params_game_action"
+
+    get "/guess_number/:theguess" => "params_game#guess_action"
+    post "guess_number" => "params_game#guess_action"
+    post "body_login" => "params_game#login_action"
   end
 end
